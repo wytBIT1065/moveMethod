ab6b61d323e8d91fc951f660de0b3144baf7e2c5
wytBIT1065 <wyuting.bit@gmail.com>
2016-10-12 17:16:50 +0800

    first commit

---
+++ RenameExpand-change/src/rename/model/DataModel.java
@@ -0,0 +1,17 @@
+package rename.model;
+
+public class DataModel {
+	
+	public String projectName = "";	
+	public String oldPackageName = "";
+	public String typeName = "";	
+	public String methodName = "";
+	public String refactorType = "";
+	public String originalName = "";
+	public String subsequentName = "";
+	public String oldClass="";
+	public String newPackageName = "";
+	public String newClass="";
+	
+
+}
---
+++ org.eclipse.ltk.core.refactoring/src/org/eclipse/ltk/core/refactoring/RenameListener.java
@@ -0,0 +1,7 @@
+package org.eclipse.ltk.core.refactoring;
+
+public interface RenameListener {
+	
+	public void onRename(RefactoringDescriptor descriptor);
+	
+}
